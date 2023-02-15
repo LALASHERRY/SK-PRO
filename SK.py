c2d62b99e9c7a947e6909e0e8d2851776e507460
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-02-15 23:33:17 +0500

    Update SK.py

--- SK.py
+++ SK.py
@@ -1,1 +1,23 @@
-import SK
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from SK64 import main
+
+    main() 
+
+elif bit == '32bit':
+
+    from SK32 import main
+
+    main()
