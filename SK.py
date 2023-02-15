1a3f50f4eeb6d4d87caa320759575b1ec62c4d11
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-02-16 00:32:31 +0500

    Update SK.py

--- SK.py
+++ SK.py
@@ -7,6 +7,6 @@ os.system('xdg-open https://facebook.com/groups/291183553213655/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import data64
+    import SK64
 elif bit == '32bit':
-    import data32
+    import SK32
