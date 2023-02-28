6c9e26d42abbf312ce59a8ee19d4869aaba84cd4
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-02-28 21:03:26 +0500

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -6,7 +6,7 @@ except:
 os.system('xdg-open https://facebook.com/groups/291183553213655/')
 
 bit = platform.architecture()[0]
-if bit == '64bit':
+elif bit == '64bit':
     import Pro64
 elif bit == '32bit':
     import Pro32
