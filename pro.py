bfc1bb8f4006cf85fbe2888cf4a0e3936c2586c4
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-03-26 23:11:38 +0500

    Update pro.py

--- pro.py
+++ pro.py
@@ -1,12 +1,13 @@
-import os, sys, platform
+import os, sys, platform,time
 try:
     import requests
 except:
     os.system('pip install requests')
+
 os.system('xdg-open https://facebook.com/groups/291183553213655/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import pro
+    import pro64
 elif bit == '32bit':
     import pro
